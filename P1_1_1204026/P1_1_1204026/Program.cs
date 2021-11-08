920bc5adbac851c9f33b399271349d94079ba15d
haryadi <haryadiyusuf61@gmail.com>
2021-11-08 19:11:39 +0700

    first commit

---
+++ P1_1_1204026/P1_1_1204026/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_1_1204026
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world from visual studio, im 1234567");
+        }
+    }
+}
